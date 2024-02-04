d12ee27b72d5a1492129fe4cb368faba03e9079b
ItzMeEriccc <133614213+EricTheWebDev@users.noreply.github.com>
2024-02-04 12:50:08 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+
+
+
+
+
+
+get_weather = input("Enter a city: ")
+
+url = f"https://open-weather13.p.rapidapi.com/city/{get_weather}"
+
+headers = {
+	"X-RapidAPI-Key": "fa30f2837cmshab533ead272d05ap1c57bcjsn8d181ef68fbf",
+	"X-RapidAPI-Host": "open-weather13.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers)
+
+print(response.json())
