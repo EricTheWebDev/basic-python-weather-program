805120b7483fcafaf79caaaa355a455175b932b7
ItzMeEriccc <133614213+EricTheWebDev@users.noreply.github.com>
2024-02-04 17:32:53 -0600

    Update app.py
    made it so that the user gets the specific temperature and what it feels like

--- app.py
+++ app.py
@@ -15,5 +15,11 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
+weather = response.json()
+if main in weather:
+	temperature = weather['main'].get("temp")
+	feels_like = weather['main'].get("feels_like")
+	print(f"temperature in {get_weather}  currently : {temperature} feels like {feels_like}")
+else:
+	print("please enter valid city")
 
-print(response.json())
