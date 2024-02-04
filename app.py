48a5d2b3685c4b82cce1655590b2745f60ac7ee5
ItzMeEriccc <133614213+EricTheWebDev@users.noreply.github.com>
2024-02-04 12:50:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ get_weather = input("Enter a city: ")
 url = f"https://open-weather13.p.rapidapi.com/city/{get_weather}"
 
 headers = {
-	"X-RapidAPI-Key": "fa30f2837cmshab533ead272d05ap1c57bcjsn8d181ef68fbf",
+	"X-RapidAPI-Key": "fa30f2837cmshab533ead272d05ap1c57bcjsn8d181ef68fbf", # use your own key
 	"X-RapidAPI-Host": "open-weather13.p.rapidapi.com"
 }
 
